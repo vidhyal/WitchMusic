f13eade0b7f6b6e0728584a29c76a16c4defa01f
vidhyal <vvl.1990@gmail.com>
2016-07-19 00:41:31 -0700

    Update FeatureExtract.py

--- FeatureExtract.py
+++ FeatureExtract.py
@@ -6,20 +6,25 @@ import tables
 
 rootdir = os.getcwd()
 newdir = os.path.join(rootdir,'data')
-fout = open('out2.txt', 'w+')
+ 
+if not os.path.exists('featurefiles'):
+    os.makedirs('featurefiles')
+outdir = os.path.join(rootdir,'featurefiles')
+fout = open(os.path.join(outdir,'out_1.txt'), 'w+')
 
-field_vector = ['tempo','loudness', 'start_of_fade_out', 'end_of_fade_in', 'key', 'key_confidence', 'mode', 'mode_confidence', 'time_signature', 'time_signature_confidence']
+field_vector = ['tempo','loudness', 'danceability', 'end_of_fade_in','key_confidence','mode_confidence']
+
 
 for subdir, dirs, files in os.walk(newdir):
     #print subdir
     for file in files:
         f = tables.openFile(os.path.join(subdir,file),'r')
-        feature_vector =[]
+   	feature_vector =[]
 	track = f.root.analysis.songs.cols.track_id[0]
         line = track
         for s in range(len(field_vector)):
             field = 'f.root.analysis.songs.cols.'+field_vector[s]+'[0]'
-            line+= " %f " %(float(eval(field)))
+            line+= " %f" %(float(eval(field)))
         line+="\n"
         fout.write(line)
         f.close()
