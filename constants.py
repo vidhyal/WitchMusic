565aab5d369739857224de98f20a7bc29d3565e5
Nandini-K <nandini2@pdx.edu>
2016-07-31 22:09:27 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 #Copyright (c) 2016 Vidhya, Nandini
+#Following code is available for use under MIT license. Please see the LICENSE file for details.
 
 genres = ['folk', 'reggae', 'punk', 'metal', 'classical', 'electronica', 'hip hop','rock', 'r&b', 'pop','jazz']
