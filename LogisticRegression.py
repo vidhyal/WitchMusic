8a2c00255e8c536cb01481929099c38d2fecba6c
Nandini-K <nandini2@pdx.edu>
2016-08-01 14:41:05 -0700

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -16,5 +16,15 @@ clf = sklearn.linear_model.LogisticRegression(tol=0.0001, fit_intercept=True, in
 clf.fit(data, labels)
 testdata = loadtxt('LR_TestFile.txt')
 h = clf.predict(testdata)
+hideal = loadtxt('LR_TestLabels.txt')
+size = '%s' %str(h.shape)
+pstr = size[1:-2]
+count = float(pstr)
 print h
+diff = h-hideal
+error = float(np.count_nonzero(diff))
+accuracy = float((count-error)/count)
+result = 'Accuracy = '
+result+= '%f' %float(accuracy)
+print result
 exit()
