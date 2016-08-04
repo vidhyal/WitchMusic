4e2d5f287c8cd0a517cb2cb6eb60055570edc347
Nandini-K <nandini2@pdx.edu>
2016-08-03 20:41:32 -0700

    Update GaussianScikit.py

--- GaussianScikit.py
+++ GaussianScikit.py
@@ -38,8 +38,6 @@ fout.close()
     
 accuracy = accuracy_score(test_labels, pred)
 print confusion_matrix(test_labels, pred)
-result = 'Accuracy of GaussianScikit ='
+result = '\n Accuracy of GaussianScikit ='
 result+= '%f' %float(accuracy)
-print result
-
-
+print result + '\n \n'
