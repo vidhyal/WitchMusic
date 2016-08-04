87a2a59edca995c73ab11f6152df7c3fd6787340
Nandini-K <nandini2@pdx.edu>
2016-08-03 17:33:38 -0700

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -24,7 +24,7 @@ print h
 diff = h-hideal
 error = float(np.count_nonzero(diff))
 accuracy = float((count-error)/count)
-result = 'Accuracy = '
+result = 'Accuracy of Logistic Regression = '
 result+= '%f' %float(accuracy)
 print result
 exit()
