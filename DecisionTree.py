cbc5a3aedb29f58ac7e587747aaae3459903dc33
Nandini-K <nandini2@pdx.edu>
2016-08-05 11:38:42 -0700

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,3 +1,6 @@
+#Copyright (c) 2016 Vidhya, Nandini
+#Following code is available for use under MIT license. Please see the LICENSE file for details.
+
 from sklearn import tree
 from BalanceData import *
 from sklearn.metrics import *
