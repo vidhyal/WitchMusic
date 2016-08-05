79105836713fee6d96e75fb0c914d1c2d6fe5649
Nandini-K <nandini2@pdx.edu>
2016-08-05 11:46:35 -0700

    Update NeuralNets.py

--- NeuralNets.py
+++ NeuralNets.py
@@ -1,3 +1,6 @@
+#Copyright (c) 2016 Vidhya, Nandini
+#Following code is available for use under MIT license. Please see the LICENSE file for details.
+
 from sknn.mlp import MultiLayerPerceptron, Classifier, Layer
 from BalanceData import *
 from constants import *
