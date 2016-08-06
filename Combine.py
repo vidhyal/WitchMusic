33b9ce74e5dcb276dffd2525e31e67237da8359b
Nandini-K <nandini2@pdx.edu>
2016-08-05 19:25:41 -0700

    Update Combine.py

--- Combine.py
+++ Combine.py
@@ -85,5 +85,6 @@ for key in range(len(test_keys)):
 fout.close()
 accuracy = accuracy_score(test_labels, pred)
 print confusion_matrix(test_labels, pred)
-print accuracy    
-
+result = '\n Accuracy of Combine Method = '
+result+= '%f' %float(accuracy) + '\n \n'
+print result
