53e1f8b93a0d9a3b5ce788eae3eb198f4ae8d0b6
vidhyal <vvl.1990@gmail.com>
2016-08-06 18:09:11 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,10 @@
 #Copyright (c) 2016 Vidhya, Nandini
 #Following code is available for use under MIT license. Please see the LICENSE file for details.
 
-#genres = ['folk', 'reggae', 'punk', 'classical', 'electronica', 'hip hop','rock', 'metal', 'pop','jazz']
+#This project uses as constant the genres we need to train with and classify into. This constant list of genres is stored in this file and is included in others for prediction.
 
-genres = [ 'reggae',   'electronica', 'hip hop','rock', 'pop','jazz']
+# This is the list of 10 genres used for classification
+genres = ['folk', 'reggae', 'punk', 'classical', 'electronica', 'hip hop','rock', 'metal', 'pop','jazz']
+
+# If using only 6 genres, comment out or remove the above line and uncomment the following line.
+#genres = [ 'reggae',   'electronica', 'hip hop','rock', 'pop','jazz']
