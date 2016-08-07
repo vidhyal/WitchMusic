c87e8a34741a2ef0238a6b355bf363e0507a4791
vidhyal <vvl.1990@gmail.com>
2016-08-06 18:04:15 -0700

    Update FeatureExtract.py

--- FeatureExtract.py
+++ FeatureExtract.py
@@ -1,6 +1,7 @@
 #Copyright (c) 2016 Vidhya, Nandini
-#Following code is available for use under MIT license. Please see the LICENSE file for details.
 
+
+#This file extracts features for each track (in the format of hdf5 files) in the recursive subdirectories of teh directory "data". 
 import os
 import h5py
 import tables
@@ -8,18 +9,23 @@ import numpy as np
 
 rootdir = os.getcwd()
 newdir = os.path.join(rootdir,'data')
- 
+
+#define the destination file to store fetaure vectors
+
 if not os.path.exists('featurefiles'):
     os.makedirs('featurefiles')
 outdir = os.path.join(rootdir,'featurefiles')
 fout = open(os.path.join(outdir,'out_1.txt'), 'w+')
 
-field_vector = ['tempo','loudness', 'danceability', 'end_of_fade_in','key','mode', 'time_signature']
+#The fields to be selected from the data files available.
+field_vector = ['tempo','loudness', 'danceability', 'end_of_fade_in','key_confidence','mode_confidence']
 TIMBRE = True
-BEATS = False #True
+BEATS = False # This is because not all of teh tracks had this feature defined and this resulted in an exception
 PITCHES = True
 SEGLOUD = True #False
 
+
+# This method returns 2 features per column of the feature denoted by field, i.e. the average and variance of the feature along that column.
 def getFieldLine(f, field, line):
     vals = eval('f.root.analysis.' +field)
     vals = np.asarray(vals)
@@ -33,11 +39,13 @@ def getFieldLine(f, field, line):
     #input ("wait")
     return line
 
+#This method returns the same as the getFieldLine method but for features that have just one column
 def getField(f, field):
-    beats = eval('f.root.analysis.' +field)
-    beats = np.asarray(beats)
-    return np.average(beats, axis=0), np.var(beats, axis=0, ddof=0.000001)
+    vals = eval('f.root.analysis.' +field)
+    vals = np.asarray(vals)
+    return np.average(vals, axis=0), np.var(vals, axis=0, ddof=0.000001)
 
+#This is the main method of this python file. It recurses through all the subdirectories of the data directory, opens each of teh data file, extracts features from that fiel, writes the feature vector to an output file and closes the data file.
 for subdir, dirs, files in os.walk(newdir):
     #print subdir
     for file in files:
@@ -50,11 +58,6 @@ for subdir, dirs, files in os.walk(newdir):
             line+= " %f" %(float(eval(field)))
         if TIMBRE:
                 line = getFieldLine(f, 'segments_timbre', line)
-		#timbre = getTimbre(f)
-		#for k in np.average(timbre, axis=0):
-		 #   line += " %f" %float(k)
-		#for k in np.var(timbre, axis=0):
-		 #   line += " %f" %float(k)
         if BEATS:
             beats_av, beats_var = getField(f, 'beats_confidence')
             line += " %f" %float(beats_av)
